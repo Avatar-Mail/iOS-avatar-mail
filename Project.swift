4b0a2e264a3592abfb198e4a50c50d71262093c4
최지석 <jiseok212@gmail.com>
2024-06-15 17:52:12 +0900

    Tuist Package 파일 내 필요한 서드파티 프레임워크 의존성 추가

--- Project.swift
+++ Project.swift
@@ -16,7 +16,28 @@ let project = Project(
             ),
             sources: ["AvatarMail/Sources/**"],
             resources: ["AvatarMail/Resources/**"],
-            dependencies: []
+            dependencies: [
+                // API
+                .external(name: "OpenAI", condition: .none),
+                // DB
+                .external(name: "Realm", condition: .none),
+                .external(name: "RealmSwift", condition: .none),
+                // Network
+                .external(name: "Alamofire", condition: .none),
+                // Rx
+                .external(name: "RxSwift", condition: .none),
+                .external(name: "RxCocoa", condition: .none),
+                .external(name: "RxOptional", condition: .none),
+                .external(name: "RxGesture", condition: .none),
+                // Architecture
+                .external(name: "ReactorKit", condition: .none),
+                // DI
+                .external(name: "Swinject", condition: .none),
+                // UI
+                .external(name: "SnapKit", condition: .none),
+                .external(name: "Then", condition: .none),
+                .external(name: "Toast", condition: .none),
+            ]
         ),
     ]
 )
