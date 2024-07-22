677ee414ec45c693a3e5146f073214cc9c5df1e6
최지석 <jiseok212@gmail.com>
2024-07-22 21:22:08 +0900

    [Test] 로컬 서버 통신 테스트
    - 텍스트 파일, 음성 파일 송수신 테스트

--- AvatarMail/Sources/Feature/Setting/SettingHomeController.swift
+++ AvatarMail/Sources/Feature/Setting/SettingHomeController.swift
@@ -187,6 +187,8 @@ class SettingHomeController: UIViewController {
     // 편지 내용
     private var mailContents: String?
     
+    private var serverSentFileURL: URL?
+    
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -575,249 +577,124 @@ class SettingHomeController: UIViewController {
             .bind { [weak self] in
                 guard let self else { return }
                 
-                let url = URL(string: "http://127.0.0.1:8000/")
-                var request = URLRequest(url: url!)
-                request.httpMethod = "GET"
-                
-                
-                let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                    guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                        print("Error: invalid response")
-                        return
+                if let url = self.serverSentFileURL {
+                    print("url: \(url)")
+                    
+                    let result = self.playingManager.startPlaying(url: url)
+                    
+                    switch result {
+                    case .success(()):
+                        print("Succeed")
+                    case .failure(let error):
+                        switch error {
+                        case .audioPlayerCreationFailure:
+                            print("audioPlayerCreationFailure")
+                        case .audioPlayerNotFound:
+                            print("audioPlayerNotFound")
+                        case .playingSessionSetupFailure:
+                            print("playingSessionSetupFailure")
+                        case .timerCreationFailure:
+                            print("timerCreationFailure")
+                        case .timerNotFound:
+                            print("timerNotFound")
+                        }
                     }
-                    print("Response : \(data!)")
+                } else {
+                    print("No saved url.")
                 }
-                task.resume()
+                
+            }.disposed(by: disposeBag)
+        
+        playServerSentFileButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                
+                if let sampleText = mailContents, let fileURL = currentRecording?.fileURL {
+                    uploadTextAndAudioFileToServer(text: sampleText, fileURL: fileURL, to: URL(string: "http://127.0.0.1:8000/upload")!)
+                } else {
+                    print("Data has not yet been set.")
+                }
                 
             }.disposed(by: disposeBag)
     }
 }
 
+extension SettingHomeController {
+    func createBody(with parameters: [String: String]?, filePathKey: String, paths: [URL], boundary: String) -> Data {
+        
+        var body = Data()
 
-//import Foundation
-//import UIKit
-//import Then
-//import SnapKit
-//import RxSwift
-//import RxCocoa
-//import ReactorKit
-//import AVFoundation
-//
-//class SettingHomeController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
-//    
-//    var disposeBag = DisposeBag()
-//    
-//    private let pageTitleLabel = UILabel().then {
-//        $0.text = "설정"
-//        $0.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-//    }
-//    
-//    private var recordButton = UIButton().then {
-//        $0.setTitle("녹음 시작", for: .normal)
-//        $0.setTitleColor(.blue, for: .normal)
-//    }
-//    
-//    private var playButton = UIButton().then {
-//        $0.setTitle("실행", for: .normal)
-//        $0.setTitleColor(.blue, for: .normal)
-//    }
-//    
-//    private var textField = UITextField().then {
-//        $0.placeholder = "파일 이름 입력"
-//        $0.borderStyle = .roundedRect
-//    }
-//    
-//    private var minuteLabel = UILabel().then {
-//        $0.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-//    }
-//    
-//    private var secondLabel = UILabel().then {
-//        $0.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-//    }
-//    
-//    private var millisecondLabel = UILabel().then {
-//        $0.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-//    }
-//    
-//    var audioRecorder: AVAudioRecorder!
-//    var audioPlayer: AVAudioPlayer!
-//    var isRecording = false
-//    var isPlaying = false
-//    
-//    init() {
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        makeUI()
-//        setupRecorder()
-//        
-//        recordButton.addTarget(self, action: #selector(recordButtonTapped), for: .touchUpInside)
-//        playButton.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
-//    }
-//    
-//    private var timer: CustomTimer?
-//    
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//
-//        timer = CustomTimer()
-//        timer?.delegate = self
-//        timer?.startTimer()
-//        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { [weak self] in
-//            self?.timer?.stopTimer()
-//        }
-//    }
-//    
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidDisappear(animated)
-//        
-//        timer?.stopTimer()
-//        timer = nil
-//    }
-//    
-//    private func makeUI() {
-//        view.backgroundColor = .white
-//        
-//        view.addSubview(pageTitleLabel)
-//        view.addSubview(recordButton)
-//        view.addSubview(textField)
-//        view.addSubview(playButton)
-//        view.addSubview(minuteLabel)
-//        view.addSubview(secondLabel)
-//        view.addSubview(millisecondLabel)
-//        
-//        // title label
-//        pageTitleLabel.snp.makeConstraints {
-//            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(35)
-//            $0.left.equalToSuperview().inset(20)
-//        }
-//        
-//        // 녹음 버튼
-//        recordButton.snp.makeConstraints {
-//            $0.top.equalTo(pageTitleLabel.snp.bottom).offset(20)
-//            $0.centerX.equalToSuperview()
-//            $0.width.equalTo(100)
-//            $0.height.equalTo(50)
-//        }
-//        
-//        // 텍스트 필드
-//        textField.snp.makeConstraints {
-//            $0.top.equalTo(recordButton.snp.bottom).offset(20)
-//            $0.centerX.equalToSuperview()
-//            $0.width.equalTo(200)
-//            $0.height.equalTo(40)
-//        }
-//        
-//        // 실행 버튼
-//        playButton.snp.makeConstraints {
-//            $0.top.equalTo(textField.snp.bottom).offset(20)
-//            $0.centerX.equalToSuperview()
-//            $0.width.equalTo(100)
-//            $0.height.equalTo(50)
-//        }
-//        
-//        minuteLabel.snp.makeConstraints {
-//            $0.top.equalTo(playButton.snp.bottom).offset(20)
-//            $0.left.equalTo(view.snp.centerX).offset(-60)
-//        }
-//        
-//        secondLabel.snp.makeConstraints {
-//            $0.top.equalTo(playButton.snp.bottom).offset(20)
-//            $0.centerX.equalToSuperview()
-//        }
-//        
-//        millisecondLabel.snp.makeConstraints {
-//            $0.top.equalTo(playButton.snp.bottom).offset(20)
-//            $0.left.equalTo(view.snp.centerX).offset(60)
-//        }
-//    }
-//    
-//    private func setupRecorder() {
-//        let settings = [
-//            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-//            AVSampleRateKey: 12000,
-//            AVNumberOfChannelsKey: 1,
-//            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-//        ]
-//        
-//        let audioSession = AVAudioSession.sharedInstance()
-//        do {
-//            try audioSession.setCategory(.playAndRecord, mode: .default)
-//            try audioSession.setActive(true)
-//            
-//            let url = getDocumentsDirectory().appendingPathComponent("a.m4a")
-//            audioRecorder = try AVAudioRecorder(url: url, settings: settings)
-//            audioRecorder.delegate = self
-//        } catch {
-//            print("Failed to setup recorder: \(error)")
-//        }
-//    }
-//    
-//    @objc func recordButtonTapped() {
-//        if isRecording {
-//            audioRecorder.stop()
-//            recordButton.setTitle("녹음 시작", for: .normal)
-//        } else {
-//            audioRecorder.record()
-//            recordButton.setTitle("녹음 종료", for: .normal)
-//        }
-//        isRecording.toggle()
-//    }
-//    
-//    @objc func playButtonTapped() {
-//        if isPlaying {
-//            audioPlayer.stop()
-//            playButton.setTitle("실행", for: .normal)
-//        } else {
-//            guard let filename = textField.text, !filename.isEmpty else { return }
-//            let url = getDocumentsDirectory().appendingPathComponent("\(filename).m4a")
-//            do {
-//                audioPlayer = try AVAudioPlayer(contentsOf: url)
-//                audioPlayer.delegate = self
-//                audioPlayer.play()
-//                playButton.setTitle("중지", for: .normal)
-//            } catch {
-//                print("Failed to play audio: \(error)")
-//            }
-//        }
-//        isPlaying.toggle()
-//    }
-//    
-//    func getDocumentsDirectory() -> URL {
-//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//        return paths[0]
-//    }
-//    
-//    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-//        playButton.setTitle("실행", for: .normal)
-//        isPlaying = false
-//    }
-//}
-//
-//
-//extension SettingHomeController: CustomTimerDelegate {
-//    
-//    func convertSecondsToTimeString(seconds: Double) -> (String, String, String) {
-//        let totalMilliseconds = Int(seconds * 1000)
-//        let minutes = (totalMilliseconds / 1000) / 60
-//        let secs = (totalMilliseconds / 1000) % 60
-//        let millisecs = (totalMilliseconds % 1000) / 10
-//        
-//        return (String(format: "%02d", minutes), String(format: "%02d", secs), String(format: "%02d", millisecs))
-//    }
-//    
-//    func timerUpdated(seconds: Double) {
-//        let (minutes, seconds, millisecs) = convertSecondsToTimeString(seconds: seconds)
-//        minuteLabel.text = minutes
-//        secondLabel.text = seconds
-//        millisecondLabel.text = millisecs
-//    }
-//}
+        if let parameters = parameters {
+            for (key, value) in parameters {
+                body.appendString("--\(boundary)\r\n")
+                body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString("\(value)\r\n")
+            }
+        }
+
+        for path in paths {
+            let filename = path.lastPathComponent
+            let data = try! Data(contentsOf: path)
+            let mimetype = "audio/m4a"
+
+            body.appendString("--\(boundary)\r\n")
+            body.appendString("Content-Disposition: form-data; name=\"\(filePathKey)\"; filename=\"\(filename)\"\r\n")
+            body.appendString("Content-Type: \(mimetype)\r\n\r\n")
+            body.append(data)
+            body.appendString("\r\n")
+        }
+
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body
+    }
+    
+    func uploadTextAndAudioFileToServer(text: String, fileURL: URL, to url: URL) {
+        
+        print(text, String(describing: fileURL))
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let boundary = UUID().uuidString
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        let parameters = ["text": text]
+        let paths = [fileURL]
+
+        request.httpBody = createBody(with: parameters, filePathKey: "file", paths: paths, boundary: boundary)
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(String(describing: error))")
+                return
+            }
+
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+
+            let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let fileURL = documentPath.appendingPathComponent("download.m4a")
+            self.serverSentFileURL = fileURL
+            
+            print("\(fileURL) saved.")
+            do {
+                try data.write(to: fileURL)
+            } catch {
+                print("no server sent fileURL")
+            }
+        }
+        task.resume()
+    }
+}
+
+
+extension Data {
+    mutating func appendString(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            append(data)
+        }
+    }
+}
