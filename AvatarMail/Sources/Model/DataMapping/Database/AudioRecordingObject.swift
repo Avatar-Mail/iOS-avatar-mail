567748bad386d944902c4b3cbb887ced4c783030
최지석 <jiseok212@gmail.com>
2024-08-17 17:58:24 +0900

    [Feature] 불필요 주석 제거

--- AvatarMail/Sources/Model/DataMapping/Database/AudioRecordingObject.swift
+++ AvatarMail/Sources/Model/DataMapping/Database/AudioRecordingObject.swift
@@ -12,7 +12,7 @@ class AudioRecordingObject: Object {
     typealias Identifier = String
     
     @Persisted var id: Identifier                     // 파일 식별자
-    @Persisted var fileName: String                   // 파일 이름 (미사용)
+    @Persisted var fileName: String                   // 파일 이름
     @Persisted var contents: String                   // 파일(음성 녹음) 내용
     @Persisted var createdDate: Date                  // 파일 생성 시간
     @Persisted var duration: Double                   // 녹음 시간
--- AvatarMail/Sources/Model/Model/AudioRecording.swift
+++ AvatarMail/Sources/Model/Model/AudioRecording.swift
@@ -11,7 +11,7 @@ struct AudioRecording: Hashable, Codable {
     typealias Identifier = String
     
     let id: Identifier            // 파일 식별자
-    let fileName: String          // 파일 이름 (미사용)
+    let fileName: String          // 파일 이름
     let contents: String          // 파일(음성 녹음) 내용
     let createdDate: Date         // 파일 생성 시간
     var duration: TimeInterval    // 녹음 시간
