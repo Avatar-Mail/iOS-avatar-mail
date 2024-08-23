7b6030983c7091524284c7ab137d31b2670923ac
최지석 <jiseok212@gmail.com>
2024-08-23 20:33:56 +0900

    [Feature] 탭바 다시 올라오지 않는 이슈 수정

--- AvatarMail/Sources/Util/Extensions/UITabBar+Extension.swift
+++ AvatarMail/Sources/Util/Extensions/UITabBar+Extension.swift
@@ -8,45 +8,68 @@
 import UIKit
 
 extension UITabBarController {
+    
     func hideTabBar(isHidden: Bool, animated: Bool) {
-        if let customTabBarController = self as? CustomTabBarController {
-            guard customTabBarController.customTabBar.isHidden != isHidden else { return }
-            let tabBar = customTabBarController.customTabBar
-            let tabBarHeight = tabBar.frame.size.height
-            let duration: TimeInterval = animated ? 0.3 : 0.0
+        guard let customTabBarController = self as? CustomTabBarController,
+              customTabBarController.customTabBar.isHidden != isHidden else { return }
+        
+        let tabBar = customTabBarController.customTabBar
+        let tabBarHeight = tabBar.frame.size.height
+        let duration: TimeInterval = animated ? 0.3 : 0.0
             
-            if isHidden {
-                UIView.animate(withDuration: duration, animations: {
-                    tabBar.frame.origin.y += tabBarHeight
-                }, completion: { _ in
-                    tabBar.isHidden = true
-                })
-            } else {
-                tabBar.isHidden = false
+        if isHidden {
+            UIView.animate(withDuration: duration, animations: {
                 tabBar.frame.origin.y += tabBarHeight
-                UIView.animate(withDuration: duration, animations: {
-                    tabBar.frame.origin.y -= tabBarHeight
-                })
-            }
+            }, completion: { _ in
+                tabBar.isHidden = true
+            })
         } else {
-            guard tabBar.isHidden != isHidden else { return }
-            let tabBar = self.tabBar
-            let tabBarHeight = tabBar.frame.size.height
-            let duration: TimeInterval = animated ? 0.3 : 0.0
-            
-            if isHidden {
-                UIView.animate(withDuration: duration, animations: {
-                    tabBar.frame.origin.y += tabBarHeight
-                }, completion: { _ in
-                    tabBar.isHidden = true
-                })
-            } else {
-                tabBar.isHidden = false
-                tabBar.frame.origin.y += tabBarHeight
-                UIView.animate(withDuration: duration, animations: {
-                    tabBar.frame.origin.y -= tabBarHeight
-                })
-            }
+            tabBar.isHidden = false
+            UIView.animate(withDuration: duration, animations: {
+                tabBar.frame.origin.y -= tabBarHeight
+            })
         }
     }
+    
+//    func hideTabBar(isHidden: Bool, animated: Bool) {
+//        if let customTabBarController = self as? CustomTabBarController {
+//            guard customTabBarController.customTabBar.isHidden != isHidden else { return }
+//            let tabBar = customTabBarController.customTabBar
+//            let tabBarHeight = tabBar.frame.size.height
+//            let duration: TimeInterval = animated ? 0.3 : 0.0
+//            
+//            if isHidden {
+//                UIView.animate(withDuration: duration, animations: {
+//                    tabBar.frame.origin.y += tabBarHeight
+//                }, completion: { _ in
+//                    tabBar.isHidden = true
+//                })
+//            } else {
+//                tabBar.isHidden = false
+//                tabBar.frame.origin.y += tabBarHeight
+//                UIView.animate(withDuration: duration, animations: {
+//                    tabBar.frame.origin.y -= tabBarHeight
+//                })
+//            }
+//        } else {
+//            guard tabBar.isHidden != isHidden else { return }
+//            let tabBar = self.tabBar
+//            let tabBarHeight = tabBar.frame.size.height
+//            let duration: TimeInterval = animated ? 0.3 : 0.0
+//            
+//            if isHidden {
+//                UIView.animate(withDuration: duration, animations: {
+//                    tabBar.frame.origin.y += tabBarHeight
+//                }, completion: { _ in
+//                    tabBar.isHidden = true
+//                })
+//            } else {
+//                tabBar.isHidden = false
+//                tabBar.frame.origin.y += tabBarHeight
+//                UIView.animate(withDuration: duration, animations: {
+//                    tabBar.frame.origin.y -= tabBarHeight
+//                })
+//            }
+//        }
+//    }
 }
